db3e39dabd153a9932319d63c4ac905a9e8884ed
Josh Myers <joshuajmyers@gmail.com>
2018-12-10 17:28:24 +0000

    Add Josh Myers user

---
+++ conf/users/josh.tf
@@ -0,0 +1,11 @@
+module "josh" {
+  source  = "git::https://github.com/cloudposse/terraform-aws-iam-user.git?ref=tags/0.1.1"
+  name    = "josh"
+  pgp_key = "keybase:josh_myers"
+  groups  = "${local.admin_groups}"
+  force_destroy = "true"
+}
+
+output "josh_decrypt_command" {
+  value = "${module.josh.keybase_password_decrypt_command}"
+}
