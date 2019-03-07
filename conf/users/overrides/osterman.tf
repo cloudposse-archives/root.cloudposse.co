21fcda8603b4e3c186cdb357ba1b25bd3fdc8b14
Erik Osterman <erik@cloudposse.com>
2019-03-07 01:55:07 -0800

    drop smtp

--- conf/users/overrides/osterman.tf
+++ conf/users/overrides/osterman.tf
@@ -9,24 +9,3 @@ module "osterman" {
 output "osterman_decrypt_command" {
   value = "${module.osterman.keybase_password_decrypt_command}"
 }
-
-module "osterman_welcome" {
-  source   = "git::https://github.com/cloudposse/terraform-null-smtp-mail.git?ref=tags/0.1.0"
-  enabled  = "false"
-  host     = "${var.smtp_host}"
-  port     = "${var.smtp_port}"
-  username = "${var.smtp_username}"
-  password = "${var.smtp_password}"
-  from     = "ops@cloudposse.com"
-  to       = ["erik@cloudposse.com"]
-  subject  = "AWS User Account Created"
-  body     = "${file("${path.module}/welcome.txt")}"
-
-  vars = {
-    signin_url               = "${local.signin_url}"
-    username                 = "${module.osterman.user_name}"
-    password_decrypt_command = "${module.osterman.keybase_password_decrypt_command}"
-  }
-}
-
-
